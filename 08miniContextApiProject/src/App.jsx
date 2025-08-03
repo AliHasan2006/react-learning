4d07086288c472314c27502b08b8e28358808fd1
AliHasan@2006 <alihasan72@gmail.com>
2025-08-03 16:09:03 +0530

    Context Api project added

--- 08miniContextApiProject/src/App.jsx
+++ 08miniContextApiProject/src/App.jsx
@@ -1,12 +1,19 @@
 
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
-
+  
 
   return (
-  <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>  )
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
 }
 
 export default App
---
+++ 08miniContextApiProject/src/context/userContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+
+
+/* this is part 1
+context always give a major thing that is provider because context give you a variable (data). so every context is/has a provider. conext is global variable. provider is use wrapper to rap componenest like fragments <> </>.    
+*/
